39b5876ff669fd9abd10a2d8f744d73b45f5126d
Jadoiconic <86121130+MustaDevPro@users.noreply.github.com>
2021-01-28 16:39:27 +0200

    commit number 700

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os 
 import random
 
-for i in range(90,300):
+for i in range(700,710):
     for j in range(0,10):
         d= str(i)
+        text= 'commit number ' + str(i)
         with open('file.txt','a') as file:
             file.write(d)
         os.system('git add .')
         # os.system('git commit -m "commit' +d + '"')
-        os.system('git commit --date="'+ d +' day ago" -m "almost done"')
+        os.system('git commit --date="'+ d +' day ago" -m "' + text +'"')
 os.system('git push')
 
 
