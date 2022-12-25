16865b35146365c4561a59e05468f56927a80ffe
Mustapha Iconic <86121130+MustaDevPro@users.noreply.github.com>
2022-12-25 17:08:10 +0200

    commit1days ago

---
+++ app.py
@@ -0,0 +1,11 @@
+import os 
+import random
+
+for i in range(1,365):
+    for j in range(0,10):
+        d= str(i) + 'days ago'
+        with open('file.txt','a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit -m "commit' +d + '"')
+os.system('git push -u origin main')
