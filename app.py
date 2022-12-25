bee7acf9e937d87f7ae0a3284c8e8716c1ab942f
Mustapha Iconic <86121130+MustaDevPro@users.noreply.github.com>
2022-12-25 19:31:20 +0200

    new cmt

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os 
 import random
 
-for i in range(1,365):
+for i in range(1,10):
     for j in range(0,10):
         d= str(i) + 'days ago'
         with open('file.txt','a') as file:
             file.write(d)
         os.system('git add .')
         os.system('git commit -m "commit' +d + '"')
-os.system('git push -u origin main')
+os.system('git push')
